752b69337abe5b5143fb5f492a040f25e3dcdd07
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-03-25 09:01:34 -0400

    updated for named args, linting, and verifying on etherscan;

--- deploy/01_Deploy_PriceConsumerV3.js
+++ deploy/01_Deploy_PriceConsumerV3.js
@@ -3,24 +3,23 @@ module.exports = async ({
     deployments,
     getChainId,
     getUnnamedAccounts,
-  }) => {
-    const {deploy} = deployments;
-    const {deployer} = await getNamedAccounts()
+}) => {
+    const { deploy } = deployments
+    const { deployer, EthUsdPriceFeed } = await getNamedAccounts()
 
     //Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
     //Default one below is ETH/USD contract on Kovan
-    const PRICE_FEED_CONTRACT="0x9326BFA02ADD2366b30bacB125260Af641031331";
 
     console.log("----------------------------------------------------")
-    console.log('Deploying PriceConsumerV3');
-      const priceConsumerV3 = await deploy('PriceConsumerV3', {
-      from: deployer,
-      gasLimit: 4000000,
-      args: [PRICE_FEED_CONTRACT],
-    });
+    console.log('Deploying PriceConsumerV3')
+    const priceConsumerV3 = await deploy('PriceConsumerV3', {
+        from: deployer,
+        gasLimit: 4000000,
+        args: [EthUsdPriceFeed],
+    })
 
     //await priceConsumerV3.deployed()
     console.log("PriceConsumerV3 deployed to: ", priceConsumerV3.address)
     console.log("Run Price Feed contract with command:")
-    console.log("npx hardhat read-price-feed --contract ",priceConsumerV3.address)
-  };
+    console.log("npx hardhat read-price-feed --contract ", priceConsumerV3.address)
+}
--- deploy/03_Deploy_RandomNumberConsumer.js
+++ deploy/03_Deploy_RandomNumberConsumer.js
@@ -1,31 +1,26 @@
 module.exports = async ({
-    getNamedAccounts,
-    deployments,
-    getChainId,
-    getUnnamedAccounts,
-  }) => {
-    const {deploy} = deployments;
-    const {deployer} = await getNamedAccounts()
-    
-    //LINK Token address set to Kovan address. Can get other values at https://docs.chain.link/docs/link-token-contracts
-    //VRF Details set for Kovan environment, can get other values at https://docs.chain.link/docs/vrf-contracts#config
-    const LINK_TOKEN_ADDR="0xa36085F69e2889c224210F603D836748e7dC0088"
-    const VRF_COORDINATOR="0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9"
-    const VRF_FEE="100000000000000000"
-    const VRF_KEYHASH="0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4"
+  getNamedAccounts,
+  deployments,
+  getChainId,
+  getUnnamedAccounts,
+}) => {
+  const { deploy } = deployments
+  const { deployer, LINK, VrfCoordinator, KeyHash, Fee } = await getNamedAccounts()
+  //LINK Token address set to Kovan address. Can get other values at https://docs.chain.link/docs/link-token-contracts
+  //VRF Details set for Kovan environment, can get other values at https://docs.chain.link/docs/vrf-contracts#config
 
-    console.log("----------------------------------------------------")
-    console.log('Deploying RandomNumberConsumer');
-      const randomNumberConsumer = await deploy('RandomNumberConsumer', {
-      from: deployer,
-      gasLimit: 4000000,
-      args: [VRF_COORDINATOR,LINK_TOKEN_ADDR,VRF_KEYHASH,VRF_FEE],
-    });
+  console.log("----------------------------------------------------")
+  console.log('Deploying RandomNumberConsumer')
+  const randomNumberConsumer = await deploy('RandomNumberConsumer', {
+    from: deployer,
+    gasLimit: 4000000,
+    args: [VrfCoordinator, LINK, KeyHash, Fee],
+  })
 
-    console.log("RandomNumberConsumer deployed to: ", randomNumberConsumer.address)
-    console.log("Run the following command to fund contract with LINK:")
-    console.log("npx hardhat fund-link --contract ",randomNumberConsumer.address);
-    console.log("Then run RandomNumberConsumer contract with the following command, replacing '777' with your chosen seed number:")
-    console.log("npx hardhat request-random-number --contract ",randomNumberConsumer.address," --seed '777'")
-    console.log("----------------------------------------------------")
-  };
+  console.log("RandomNumberConsumer deployed to: ", randomNumberConsumer.address)
+  console.log("Run the following command to fund contract with LINK:")
+  console.log("npx hardhat fund-link --contract ", randomNumberConsumer.address)
+  console.log("Then run RandomNumberConsumer contract with the following command, replacing '777' with your chosen seed number:")
+  console.log("npx hardhat request-random-number --contract ", randomNumberConsumer.address, " --seed '777'")
+  console.log("----------------------------------------------------")
+}
