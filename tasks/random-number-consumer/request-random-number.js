175de3840b9618df0bdc92646ebd34cde09d95be
pappas999 <harry@genesisblockchain.com.au>
2021-06-25 22:06:11 +0930

    remove semi-colon

--- tasks/random-number-consumer/request-random-number.js
+++ tasks/random-number-consumer/request-random-number.js
@@ -13,7 +13,7 @@ task("request-random-number", "Requests a random number for a Chainlink VRF enab
 
         //Create connection to VRF Contract and call the getRandomNumber function
         const vrfConsumerContract = new ethers.Contract(contractAddr, RandomNumberConsumer.interface, signer)
-        var result = await vrfConsumerContract.getRandomNumber();
+        var result = await vrfConsumerContract.getRandomNumber()
         console.log('Contract ', contractAddr, ' random number request successfully called. Transaction Hash: ', result.hash)
         console.log("Run the following to read the returned random number:")
         console.log("npx hardhat read-random-number --contract " + contractAddr + " --network " + network.name)
