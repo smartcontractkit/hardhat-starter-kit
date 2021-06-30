e8b03ff1c263837a8a3514c14b644ec193607b58
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-06-30 12:25:38 -0400

    removing seed reference

--- deploy/04_Setup_Contracts.js
+++ deploy/04_Setup_Contracts.js
@@ -1,5 +1,5 @@
 const { networkConfig, autoFundCheck } = require('../helper-hardhat-config')
-const {ethers, getNamedAccounts} = require('hardhat')
+const { ethers, getNamedAccounts } = require('hardhat')
 
 module.exports = async ({
   getNamedAccounts,
@@ -29,7 +29,7 @@ module.exports = async ({
   const apiConsumer = await ethers.getContractAt('APIConsumer', APIConsumer.address)
 
   if (await autoFundCheck(apiConsumer.address, networkName, linkTokenAddress, additionalMessage)) {
-    await hre.run("fund-link",{contract: apiConsumer.address, linkaddress: linkTokenAddress})
+    await hre.run("fund-link", { contract: apiConsumer.address, linkaddress: linkTokenAddress })
   } else {
     log("Then run API Consumer contract with following command:")
     log("npx hardhat request-data --contract " + apiConsumer.address + " --network " + networkName)
@@ -43,10 +43,10 @@ module.exports = async ({
   const randomNumberConsumer = await ethers.getContractAt('RandomNumberConsumer', RandomNumberConsumer.address)
 
   if (await autoFundCheck(apiConsumer.address, networkName, linkTokenAddress, additionalMessage)) {
-    await hre.run("fund-link",{contract: randomNumberConsumer.address, linkaddress: linkTokenAddress})
+    await hre.run("fund-link", { contract: randomNumberConsumer.address, linkaddress: linkTokenAddress })
   } else {
-    log("Then run RandomNumberConsumer contract with the following command, replacing '777' with your chosen seed number:")
-    log("npx hardhat request-random-number --contract " + randomNumberConsumer.address, " --seed '777'" + " --network " + networkName)
+    log("Then run RandomNumberConsumer contract with the following command:")
+    log("npx hardhat request-random-number --contract " + randomNumberConsumer.address + " --network " + networkName)
   }
   log("----------------------------------------------------")
 
