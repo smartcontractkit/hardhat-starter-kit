deb5da372df0e81304e4b9d4fe49fea2856a3b50
pappas999 <harry@genesisblockchain.com.au>
2021-09-02 17:46:03 +0930

    added tasks back in to starter kit

---
+++ tasks/api-consumer/request-data.js
@@ -0,0 +1,23 @@
+let { networkConfig, getNetworkIdFromName } = require('../../helper-hardhat-config')
+
+task("request-data", "Calls an API Consumer Contract to request external data")
+    .addParam("contract", "The address of the API Consumer contract that you want to call")
+    .setAction(async taskArgs => {
+
+        const contractAddr = taskArgs.contract
+        let networkId = await getNetworkIdFromName(network.name)
+        console.log("Calling API Consumer contract ", contractAddr, " on network ", network.name)
+        const APIConsumer = await ethers.getContractFactory("APIConsumer")
+
+        //Get signer information
+        const accounts = await ethers.getSigners()
+        const signer = accounts[0]
+
+        //Create connection to API Consumer Contract and call the createRequestTo function
+        const apiConsumerContract = new ethers.Contract(contractAddr, APIConsumer.interface, signer)
+        var result = await apiConsumerContract.requestVolumeData()
+        console.log('Contract ', contractAddr, ' external data request successfully called. Transaction Hash: ', result.hash)
+        console.log("Run the following to read the returned result:")
+        console.log("npx hardhat read-data --contract " + contractAddr + " --network " + network.name)
+    })
+module.exports = {}
---
+++ tasks/price-consumer/read-price-feed.js
@@ -0,0 +1,20 @@
+task("read-price-feed", "Gets the latest price from a Chainlink Price Feed")
+    .addParam("contract", "The address of the Price Feed consumer contract that you want to read")
+    .setAction(async taskArgs => {
+
+        const contractAddr = taskArgs.contract
+        const networkId = network.name
+
+        const PriceFeedConsumerContract = await ethers.getContractFactory("PriceConsumerV3")
+        console.log("Reading data from Price Feed consumer contract ", contractAddr, " on network ", networkId)
+
+        //Get signer information
+        const accounts = await ethers.getSigners()
+        const signer = accounts[0]
+        const priceFeedConsumerContract = await new ethers.Contract(contractAddr, PriceFeedConsumerContract.interface, signer)
+        await priceFeedConsumerContract.getLatestPrice().then((data) => {
+            console.log('Price is: ', BigInt(data).toString())
+        })
+    })
+
+module.exports = {}
