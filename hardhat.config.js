224e9f2b4bbd9cb6b646e64d7eef44b60182c03a
pappas999 <harry@genesisblockchain.com.au>
2021-10-12 16:18:14 +1030

    added fund task plugin back in,
    also set private_key as the default .env var to use, mnemonic is too revealing

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,6 +14,7 @@ require("./tasks/price-consumer")
 require("./tasks/random-number-consumer")
 require("./tasks/price-consumer")
 require("./tasks/api-consumer")
+require("@appliedblockchain/chainlink-plugins-fund-link")
 
 require('dotenv').config()
 
@@ -40,10 +41,10 @@ module.exports = {
         },
         kovan: {
             url: KOVAN_RPC_URL,
-            // accounts: [PRIVATE_KEY],
-            accounts: {
-                mnemonic: MNEMONIC,
-            },
+             accounts: [PRIVATE_KEY],
+            //accounts: {
+           //     mnemonic: MNEMONIC,
+           // },
             saveDeployments: true,
         },
         rinkeby: {
