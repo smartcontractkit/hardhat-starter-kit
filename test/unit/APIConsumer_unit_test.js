bbc6bebcb6e94cfdf9d04fd26ca264bae85c2b9c
Roberto García <roberto@rhizomik.net>
2021-12-03 11:30:28 +0100

    Full APIConsumer unit test including mocked request fulfillment

--- test/unit/APIConsumer_unit_test.js
+++ test/unit/APIConsumer_unit_test.js
@@ -4,14 +4,15 @@ const chai = require('chai')
 const { expect } = require('chai')
 const BN = require('bn.js')
 const { getChainId } = require('hardhat')
+const {numToBytes32} = require("@chainlink/test-helpers/dist/src/helpers");
 chai.use(require('chai-bn')(BN))
 
 skip.if(!developmentChains.includes(network.name)).
   describe('APIConsumer Unit Tests', async function () {
 
-    let apiConsumer, linkToken
+    let apiConsumer, linkToken, mockOracle
 
-    beforeEach(async () => {
+    before(async () => {
       const chainId = await getChainId()
       await deployments.fixture(['mocks', 'api'])
       const LinkToken = await deployments.get('LinkToken')
@@ -27,6 +28,14 @@ skip.if(!developmentChains.includes(network.name)).
       if (await autoFundCheck(apiConsumer.address, networkName, linkTokenAddress, additionalMessage)) {
         await hre.run("fund-link", { contract: apiConsumer.address, linkaddress: linkTokenAddress })
       }
+
+      const MockOracle = await deployments.get("MockOracle");
+      mockOracle = await ethers.getContractAt("MockOracle", MockOracle.address);
+    })
+
+    afterEach(async () => {
+      mockOracle.removeAllListeners()
+      apiConsumer.removeAllListeners()
     })
 
     it('Should successfully make an API request', async () => {
@@ -37,4 +46,21 @@ skip.if(!developmentChains.includes(network.name)).
       console.log("requestId: ", requestId)
       expect(requestId).to.not.be.null
     })
+
+    it("Should successfully make an API request and get a result", (done) => {
+      mockOracle.once("OracleRequest",
+        (_specId, _sender, requestId, _payment, _cbAddress, _callbackFuncId, expiration, _dataVersion, _data) => {
+          console.log("OracleRequest:", requestId, _data);
+          // Mock the fulfillment of the request
+          mockOracle.fulfillOracleRequest(requestId, numToBytes32(1));
+          // Now check the result
+          apiConsumer.volume().then((result) => {
+            console.log("API Consumer Volume: ", new ethers.BigNumber.from(result._hex).toString());
+            expect(new ethers.BigNumber.from(result._hex).toString())
+              .to.be.a.bignumber.that.is.greaterThan(new ethers.BigNumber.from(0).toString());
+            done();
+          });
+      });
+      apiConsumer.requestVolumeData();
+    });
   })
