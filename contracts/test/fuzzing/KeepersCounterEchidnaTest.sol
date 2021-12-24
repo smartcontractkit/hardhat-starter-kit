408deaaf144ace0e9a69b85df6cb9aea9e080ef4
Giovanni Di Siena <giodisiena@gmail.com>
2021-12-24 20:26:16 +0000

    Add keepers example using function selector checkData

--- contracts/test/fuzzing/KeepersCounterEchidnaTest.sol
+++ contracts/test/fuzzing/KeepersCounterEchidnaTest.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import "../../KeepersCounter.sol";
 
 contract KeepersCounterEchidnaTest is KeepersCounter {
-  constructor() KeepersCounter(8 days) {}
+  constructor() KeepersCounter(8 days, true) {}
 
   function echidna_test_perform_upkeep_gate() public view returns (bool) {
     return counter == 0;
