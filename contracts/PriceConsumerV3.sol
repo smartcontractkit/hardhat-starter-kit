23e44048dbea130c1e6349b4d23e1b2b3f9c0606
andrejrakic <andrej.rakic@elfak.rs>
2022-02-05 11:48:11 +0100

    Add NatSpec [fixes #55]

--- contracts/PriceConsumerV3.sol
+++ contracts/PriceConsumerV3.sol
@@ -1,31 +1,28 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+/**
+ * @title The PriceConsumerV3 contract
+ * @notice Acontract that returns latest price from Chainlink Price Feeds
+ */
 contract PriceConsumerV3 {
-
-    AggregatorV3Interface internal priceFeed;
-
     /**
+     * @notice Returns the latest price
+     *
+     * @param _priceFeed - Price Feed Address
+     *
      * Network: Kovan
      * Aggregator: ETH/USD
      * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     *
+     * @return latest price
      */
-    constructor(address _priceFeed) public {
-        priceFeed = AggregatorV3Interface(_priceFeed);
-    }
-
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice() public view returns (int) {
-        (
-            uint80 roundID,
-            int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
+    function getLatestPrice(address _priceFeed) public view returns (int256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(_priceFeed);
+        (uint80 roundID, int256 price, uint256 startedAt, uint256 timeStamp, uint80 answeredInRound) = priceFeed
+            .latestRoundData();
         return price;
     }
 }
--- contracts/RandomNumberConsumer.sol
+++ contracts/RandomNumberConsumer.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
@@ -7,35 +8,38 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
  * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
  */
 contract RandomNumberConsumer is VRFConsumerBase {
-
     bytes32 internal keyHash;
     uint256 internal fee;
 
     uint256 public randomResult;
 
     /**
-     * Constructor inherits VRFConsumerBase
+     * @notice Constructor inherits VRFConsumerBase
+     *
+     * @param _vrfCoordinator - address of the Chainlink VRF Coordinator
+     * @param _link - LINK token address on the corresponding network
+     * @param _keyHash - public key against which randomness is generated
+     * @param _fee - fee required to fulfill a VRF request
      *
      * Network: Kovan
      * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
      * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
      * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
      */
-    constructor(address _vrfCoordinator,
-                address _link,
-                bytes32 _keyHash,
-                uint _fee)
-        VRFConsumerBase(
-            _vrfCoordinator, // VRF Coordinator
-            _link  // LINK Token
-        ) public
-    {
+    constructor(
+        address _vrfCoordinator,
+        address _link,
+        bytes32 _keyHash,
+        uint256 _fee
+    ) VRFConsumerBase(_vrfCoordinator, _link) {
         keyHash = _keyHash;
         fee = _fee;
     }
 
     /**
-     * Requests randomness
+     * @notice Requests randomness
+     *
+     * @return requestId - id of the request
      */
     function getRandomNumber() public returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
@@ -43,11 +47,18 @@ contract RandomNumberConsumer is VRFConsumerBase {
     }
 
     /**
-     * Callback function used by VRF Coordinator
+     * @notice Callback function used by VRF Coordinator
+     *
+     * @param requestId - id of the request
+     * @param randomness - random result from VRF Coordinator
      */
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         randomResult = randomness;
     }
 
-    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+    /**
+     * @notice Witdraws LINK from the contract
+     * @dev Implement a withdraw function to avoid locking your LINK in the contract
+     */
+    function withdrawLink() external {}
 }
