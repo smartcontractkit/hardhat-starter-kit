c75916f80513a78ef0e05049ba4e4b27a2192d9c
andrejrakic <andrej.rakic@smartcontract.com>
2022-09-16 00:14:32 +0200

    Deprecate Rinkeby and Kovan support

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -16,28 +16,6 @@ const networkConfig = {
     keepersUpdateInterval: "30",
     ethUsdPriceFeed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
   },
-  42: {
-    name: "kovan",
-    linkToken: "0xa36085F69e2889c224210F603D836748e7dC0088",
-    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
-    oracle: "0xc57b33452b4f7bb189bb5afae9cc4aba1f7a4fd8",
-    jobId: "d5270d1c311941d0b08bead21fea7747",
-    fee: "100000000000000000",
-    fundAmount: "100000000000000000", // 0.1
-    keepersUpdateInterval: "30",
-  },
-  4: {
-    name: "rinkeby",
-    linkToken: "0x01be23585060835e02b77ef475b0cc51aa1e0709",
-    ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-    keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-    vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-    oracle: "0xc57b33452b4f7bb189bb5afae9cc4aba1f7a4fd8",
-    jobId: "6b88e0402e5d415eb946e528b8e0c7ba",
-    fee: "100000000000000000",
-    fundAmount: "100000000000000000", // 0.1
-    keepersUpdateInterval: "30",
-  },
   1: {
     name: "mainnet",
     linkToken: "0x514910771af9ca656af840dff83e8264ecf986ca",
@@ -47,7 +25,14 @@ const networkConfig = {
   5: {
     name: "goerli",
     linkToken: "0x326c977e6efc84e512bb9c30f76e30c160ed06fb",
-    fundAmount: "0",
+    ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+    vrfCoordinator: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    oracle: "0xCC79157eb46F5624204f47AB42b3906cAA40eaB7",
+    jobId: "ca98366cc7314957b8c012c72f05aeeb",
+    fee: "100000000000000000",
+    fundAmount: "100000000000000000", // 0.1
+    keepersUpdateInterval: "30",
   },
   137: {
     name: "polygon",
