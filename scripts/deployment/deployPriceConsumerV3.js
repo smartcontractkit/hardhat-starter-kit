a14f5da334a1f26cee19d3e79a62fa9f074d4b02
andrejrakic <andrej.rakic@smartcontract.com>
2022-11-01 20:34:21 +0100

    Fix read-price-feed task

--- scripts/deployment/deployPriceConsumerV3.js
+++ scripts/deployment/deployPriceConsumerV3.js
@@ -6,10 +6,22 @@ const {
 } = require("../../helper-hardhat-config")
 
 async function deployPriceConsumerV3(chainId) {
-    const priceFeed = networkConfig[chainId]["ethUsdPriceFeed"]
+    let priceFeedAddress;
+
+    if (developmentChains.includes(network.name)) {
+        const DECIMALS = "18"
+        const INITIAL_PRICE = "200000000000000000000"
+
+        const mockV3AggregatorFactory = await ethers.getContractFactory("MockV3Aggregator")
+        const mockV3Aggregator = await mockV3AggregatorFactory.deploy(DECIMALS, INITIAL_PRICE)
+
+        priceFeedAddress = mockV3Aggregator.address;
+    } else {
+        priceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
 
     const priceConsumerV3Factory = await ethers.getContractFactory("PriceConsumerV3")
-    const priceConsumerV3 = await priceConsumerV3Factory.deploy(priceFeed)
+    const priceConsumerV3 = await priceConsumerV3Factory.deploy(priceFeedAddress)
 
     const waitBlockConfirmations = developmentChains.includes(network.name)
         ? 1
@@ -21,7 +33,7 @@ async function deployPriceConsumerV3(chainId) {
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         await run("verify:verify", {
             address: priceConsumerV3.address,
-            constructorArguments: [priceFeed],
+            constructorArguments: [priceFeedAddress],
         })
     }
 }
