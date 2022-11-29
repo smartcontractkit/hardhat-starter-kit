6050c038bd3294f265a3248c3d3078b4333be538
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2022-11-29 17:17:34 -0600

    still wip

--- tasks/on-demand-api-consumer/whitelist.js
+++ tasks/on-demand-api-consumer/whitelist.js
@@ -1,4 +1,4 @@
-const { networkConfig } = require("../../helper-hardhat-config")
+const { networkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../../helper-hardhat-config")
 
 task("on-demand-whitelist", "Whitelists a wallet to be able to make on-demand requests")
     .addParam(
@@ -10,10 +10,14 @@ task("on-demand-whitelist", "Whitelists a wallet to be able to make on-demand re
 
         const oracle = await ethers.getContractAt("OCR2DROracle", networkConfig[5]["ocr2odOracle"])
 
+        console.log(`Whitelisting sender wallet ${walletAddr} for OCR2DROracle ${networkConfig[5]["ocr2odOracle"]}`)
         const whitelistTx = await oracle.addSender(walletAddr)
+        const waitBlockConfirmations = developmentChains.includes(network.name)
+            ? 1
+            : VERIFICATION_BLOCK_CONFIRMATIONS
+        console.log(`Waiting ${waitBlockConfirmations} blocks for transaction ${whitelistTx.hash} to be confirmed...`)
+        await whitelistTx.wait(waitBlockConfirmations)
 
-        console.log(whitelistTx)
-
-        console.log('Sender wallet whitelisted')
+        console.log(`Sender wallet ${walletAddr} whitelisted for OCR2DROracle ${networkConfig[5]["ocr2odOracle"]}`)
     })
 module.exports = {}
