17f1f70f6066ad1d33e871a2dc56db05bc72291a
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2022-12-01 10:55:53 -0600

    added newline to simulator log

--- scripts/simulateOnDemandRequest.js
+++ scripts/simulateOnDemandRequest.js
@@ -3,5 +3,5 @@ const { simulateRequest } = require('./onDemandRequestSimulator');
 (async () => {
   const { resultLog } = await simulateRequest('../../on-demand-request-config.js')
   
-  console.log(resultLog)
+  console.log(`\n${resultLog}`)
 })()
