be823486ffa4d6d27d59a3108833ceec7a7184b4
Bolek Kulbabinski <1416262+bolekk@users.noreply.github.com>
2022-12-15 12:14:19 -0800

    Format the rest of the contracts to match chainlink repo

--- contract-diff.sh
+++ contract-diff.sh
@@ -7,3 +7,17 @@ do
 	diff ${f} ~/chainlink/contracts/src/v0.8/dev/ocr2dr/${f}
 done
 
+cd ../ocr2
+for f in *
+do
+	echo "\n\n######################## DIFF for ${f}"
+	diff ${f} ~/chainlink/contracts/src/v0.8/dev/ocr2/${f}
+done
+
+cd ../interfaces
+for f in *
+do
+	echo "\n\n######################## DIFF for ${f}"
+	diff ${f} ~/chainlink/contracts/src/v0.8/dev/interfaces/${f}
+done
+
