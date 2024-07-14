64ba36e727c09455c5308146d6f802c798df9cb7
Zubin Pratap <zubin.pratap@gmail.com>
2024-07-15 09:54:04 +1000

    fix issue https://github.com/smartcontractkit/hardhat-starter-kit/issues/190.  Unit tests work but VRFv2.5 fails on coverage related unit tests?

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,9 @@
 module.exports = {
-    skipFiles: ['test/fuzzing/AutomationCounterEchidnaTest.sol', 'test/MockLinkToken.sol', 'test/MockOracle.sol', 'test/MockV3Aggregator.sol', 'test/VRFCoordinatorV2Mock.sol'],
+    skipFiles: [
+        'test/fuzzing/AutomationCounterEchidnaTest.sol', 
+        'test/MockLinkToken.sol', 'test/MockOracle.sol', 
+        'test/MockV3Aggregator.sol', 
+        'test/VRFCoordinatorV2Mock.sol'
+    ],
+    configureYulOptimizer: true
 };
